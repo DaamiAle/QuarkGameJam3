836fcc37180c238f09efedca1882a5c233595f98
DaamiAle <daamiale.recoil@gmail.com>
2023-04-14 06:04:55 -0300

    Se cambia de ubicacion archivo de jugador. Se le agrega un metodo inicializador y se le relaciona una entidad movible.

---
+++ src/Jugador.cs
@@ -0,0 +1,31 @@
+﻿
+namespace QuarkGameJam3.src
+{
+    public abstract class Jugador
+    {
+        /// <summary>
+        /// Atributo que representa la entidad del jugador.
+        /// </summary>
+        private static Movible entidad;
+        /// <summary>
+        /// Inicializa al jugador
+        /// </summary>
+        /// <param name="posicionInicial">Coordenadas donde va a iniciar el jugador</param>
+        public static void Inicializar(Coordenadas posicionInicial)
+        {
+            entidad = new Movible(posicionInicial, "@");
+            Console.SetCursorPosition(entidad.Posicion().X, entidad.Posicion().Y);
+            Console.Write(entidad.Repr());
+        }
+        /// <summary>
+        /// Mueve al jugar segun la direccion deseada.
+        /// </summary>
+        /// <param name="direccion">Direccion en la que se va a mover el jugador</param>
+        public static void Mover(Direccion direccion)
+        {
+            // Acá va una verificacion de si en la direccion que se quiere mover hay una caja
+
+            entidad.Mover(direccion);
+        }
+    }
+}
