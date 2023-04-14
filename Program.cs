ae0d2ef4281cbc9c0762c6445ae6dfbadb662e93
DaamiAle <daamiale.recoil@gmail.com>
2023-04-14 06:03:21 -0300

    Control de eventos de movimiento.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QuarkGameJam3.src;
 
 namespace QuarkGameJam3
 {
@@ -10,6 +6,68 @@ namespace QuarkGameJam3
     {
         static void Main(string[] args)
         {
+            bool finalizar = false;
+            Console.CursorVisible = false;
+            Console.Title = "Quark Game Jam 3";
+            //Jugador.Inicializar(new Coordenadas(1, 5)); //
+            
+            
+            // Renderizar Menu Principal
+            
+            while (!finalizar)// Este bucle se encarga de mantener el programa corriendo
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    
+                    // Aca el control de eventos del menu
+                    if (key.Key == ConsoleKey.UpArrow)
+                    {
+                        // Movimiento dentro del menu hacia arriba
+                    }
+                    else if (key.Key == ConsoleKey.DownArrow)
+                    {
+                        // Movimiento dentro del menu hacia abajo
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        // Aca hay que verificar las coordenadas del cursor para saber
+                        // que opcion esta seleccionada y desencadenarla
+                    }
+
+                    
+                    bool salirAlMenuPrincipal = true;
+                    // este sería el control de eventos de tecla dentro del nivel
+                    // haciendo que cuando se precione Esc se salga al menu
+
+                    while (!salirAlMenuPrincipal) 
+                    {
+                        if (key.Key == ConsoleKey.UpArrow)
+                        {
+                            Jugador.Mover(Direccion.Arriba);
+                        }
+                        else if (key.Key == ConsoleKey.RightArrow)
+                        {
+                            Jugador.Mover(Direccion.Derecha);
+                        }
+                        else if (key.Key == ConsoleKey.DownArrow)
+                        {
+                            Jugador.Mover(Direccion.Abajo);
+                        }
+                        else if (key.Key == ConsoleKey.LeftArrow)
+                        {
+                            Jugador.Mover(Direccion.Izquierda);
+                        }
+                        else if (key.Key == ConsoleKey.Escape)
+                        {
+                            salirAlMenuPrincipal = true;
+                            // Renderizar Menu Principal
+                        }
+                    }
+                     
+                    
+                }
+            }
         }
     }
 }
