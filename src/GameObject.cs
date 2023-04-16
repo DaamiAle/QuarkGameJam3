ca8748ef96494fc08278959503254e9d3de3d70e
DaamiAle <daamiale.recoil@gmail.com>
2023-04-16 13:09:53 -0300

    Se separo las clases GameObject y Movible en 2 archivos. Se agrego un 'constructor de clase base' a GameObject

--- src/GameObject.cs
+++ src/GameObject.cs
@@ -8,30 +8,11 @@ namespace QuarkGameJam3.src
         public Coordenadas Posicion() => posicion;
         public string Repr() => _repr ?? "null";
 
-
-    }
-    public class Movible : GameObject
-    {
-        public Movible(Coordenadas pos, string repr)
+        public GameObject(Coordenadas pos, string repr)
         {
             posicion = pos;
             _repr = repr;
         }
-        /// <summary>
-        /// Mueve a la entidad en la direccion dada.
-        /// Verifica que este dentro de los limites del tablero.
-        /// </summary>
-        /// <param name="direccion">Direccion en la que se desea mover</param>
-        public void Mover(Direccion direccion)
-        {
-            bool sePuedeMover = false;
-            if (Utilidades.Between(posicion.X, 1, 19) && Utilidades.Between(posicion.Y, 1, 9)) sePuedeMover = true;
-            else if (direccion == Direccion.Arriba && posicion.Y > 0) sePuedeMover = true;
-            else if (direccion == Direccion.Derecha && posicion.X < 20) sePuedeMover = true;
-            else if (direccion == Direccion.Abajo && posicion.Y < 10) sePuedeMover = true;
-            else if (direccion == Direccion.Izquierda && posicion.X > 0) sePuedeMover = true;
-
-            if (sePuedeMover) Engine.Mover(this, direccion);
-        }
     }
+    
 }
